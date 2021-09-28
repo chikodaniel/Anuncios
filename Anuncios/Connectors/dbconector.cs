65dfc3a7bec0d6af6b87d1eefc36d1ab59e958f5
Daniel Lopez <chicodanielxd@hotmail.com>
2021-09-28 13:25:35 -0500

    Se hiciero ncambios

--- Anuncios/Connectors/dbconector.cs
+++ Anuncios/Connectors/dbconector.cs
@@ -13,25 +13,25 @@ namespace Anuncios.Connectors
     {
         public AnuncioCollection listarAnuncios()
         {
-            string API_URL = "https://jsonplaceholder.typicode.com/comments?_limit=500";
-            var client = new WebClient();
-            var json = client.DownloadString(API_URL);
-            dynamic posts = JsonConvert.DeserializeObject(json);
+            //string API_URL = "https://jsonplaceholder.typicode.com/comments?_limit=500";
+            //var client = new WebClient();
+            //var json = client.DownloadString(API_URL);
+            //dynamic posts = JsonConvert.DeserializeObject(json);
            
             AnuncioCollection lista = new AnuncioCollection();
-            foreach (var post in posts)
-            {
-                string cadena1 = post["id"];
-                string cadena2 = post["name"];
-                string cadena3 = post["body"];
-                lista.Add(new Anuncio(cadena1, cadena2, cadena3));
+            //foreach (var post in posts)
+            //{
+            //    string cadena1 = post["id"];
+            //    string cadena2 = post["name"];
+            //    string cadena3 = post["body"];
+            //    lista.Add(new Anuncio(cadena1, cadena2, cadena3));
 
-            }
-            lista.Add(new Anuncio("1", "Daniel", "Hola Mexico"));
-            lista.Add(new Anuncio("2", "Ricardo", "Hola Xalapa"));
-            lista.Add(new Anuncio("3", "Juan", "Hola Veracruz"));
-            lista.Add(new Anuncio("4", "Ernesto", "Hola Tlaxcala"));
-            lista.Add(new Anuncio("5", "Gustavo", "Hola Japón"));
+            //}
+            lista.Add(new Anuncio("1", "Daniel", "Azul"));
+            lista.Add(new Anuncio("2", "Ricardo", "Rojo"));
+            lista.Add(new Anuncio("3", "Juan", "Amarillo"));
+            lista.Add(new Anuncio("4", "Ernesto", "Azul"));
+            lista.Add(new Anuncio("5", "Gustavo", "Rojo"));
             return lista;
         }
     }
---
+++ Anuncios/Core/BrushConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Anuncios.Core
+{
+    public class BrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string input = value as string;
+            switch (input)
+            {
+                case "Rojo":
+                    return Brushes.Red;
+                case "Azul":
+                    return Brushes.Blue;
+                case "Amarillo":
+                    return Brushes.Yellow;
+                default:
+                    return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
